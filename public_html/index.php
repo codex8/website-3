d597066f1d12fce8576c7950274ba9ae750adc9f
Ciaran Rooney <ciaran.rooney@gmail.com>
2012-09-03 16:39:53 +0200

    Update public_html/index.php

--- public_html/index.php
+++ public_html/index.php
@@ -2,20 +2,9 @@
 date_default_timezone_set('Europe/London');
 require('../libs/Smarty.class.php');
 
-class EvaledFileResource extends Smarty_Internal_Resource_File { 
-    public function populate(Smarty_Template_Source $source, Smarty_Internal_Template $_template=null) { 
-        parent::populate($source, $_template); 
-        $source->recompiled = true; 
-    } 
-}
-
-echo "<!-- orchestra.io test 2 -->";
+echo "<!-- orchestra.io test 3 -->";
 
 $smarty = new Smarty;
-
-$smarty 
-    ->setTemplateDir('./templates') 
-    ->registerResource('file', new EvaledFileResource()); 
 
 $menu = strtolower(filter_input(INPUT_GET, 'menu', FILTER_SANITIZE_STRING));
 if(empty($menu)) { $menu = "home"; }
@@ -25,7 +14,7 @@ $smarty->assign("menu", $menu);
 $smarty->force_compile = false;
 $smarty->debugging = false;
 $smarty->caching = false;
-//marty->cache_lifetime = 120;
+//$smarty->cache_lifetime = 120;
 
 $smarty->display('header.tpl');
 
