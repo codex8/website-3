701805a5fe4e8d25af8fe7d39ef3933f3b0932e5
zoe slattery <zoe@php.net>
2012-11-21 10:03:23 +0000

    Correct the links to articles of association and constitution

--- page-about.php
+++ page-about.php
@@ -14,7 +14,7 @@ get_header();
 		<p>PHP London is a non-profit group to promote knowledge sharing and best practice amongst PHP developers and web dev and business professionals in London. The main product right now is a regular PHP meetup - basically a monthly PHP PubCon - which is held on the first Thursday of every month. We also hold an annual conference (usually on the last Friday of February each year).</p>
 		<p>Meet PHP users, developers and recruiters near you! Come to PHP London to exchange ideas and talk about code, architecture, innovation, and open source in general. Anyone interested in PHP is welcome!</p>
 		<p>PHP London has been set up as a limited liability UK company since December 2008, primarily to protect the executive committee members' liability should money be lost on a conference event. The company's registration number is 6770512 and its VAT number is 943 4927 00.</p>
-		<p>PHP London limited is governed by its Articles of association, available <a href="http://wiki.phplondon.org/PHP-London-Articles-of-association.pdf" target="_blank">here</a>, and the day to day running of PHP London is guided by its Constitution, available <a href="http://wiki.phplondon.org/PHP-London-Constitution.pdf" target="_blank">here</a>.</p>
+		<p>PHP London limited is governed by its Articles of association, available <a href="<?php echo home_url(); ?>/PHP-London-Articles-of-association.pdf" target="_blank">here</a>, and the day to day running of PHP London is guided by its Constitution, available <a href="<?php echo home_url(); ?>/PHP-London-Constitution.pdf" target="_blank">here</a>.</p>
 		<p>&nbsp;</p>
 		<p>&nbsp;</p>
 			
